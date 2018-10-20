6313814759c4e374c7a265beb6ba26b575103f25
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-10-20 15:54:34 +0200

    Create resource page

---
+++ src/components/ResourceCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <v-layout row wrap>
+      <!--<v-flex xs12>-->
+        <!--<h2>{{ data.name }}</h2>-->
+      <!--</v-flex>-->
+      <!--<v-flex md3>-->
+        <!--<img :src="data.media.mainImg" height="150px"/>-->
+      <!--</v-flex>-->
+      <!--<v-flex md9>-->
+        <!--<div>-->
+          <!--<div class="headline"> {{ data.title }}</div>-->
+          <!--<div>{{ data.shortDesc }}</div>-->
+        <!--</div>-->
+      <!--</v-flex>-->
+      <div class="resource">
+        <div class="resource-img">
+          <img :src="data.media.mainImg" height="150px"/>
+        </div>
+        <div class="resource-content">
+          <div class="headline"> {{ data.title }}</div>
+          <div>{{ data.shortDesc }}</div>
+        </div>
+      </div>
+    </v-layout>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'ResourceCard',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/components/Resources.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <template v-for="resource in resources">
+          <ResourceCard :data="resource" :key="resource.id"/>
+        </template>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import ResourceCard from './ResourceCard'
+
+export default {
+  components: { ResourceCard },
+  data: () => ({
+    resources: [
+      {
+        id: '1234567',
+        name: 'Recurso 1',
+        shortDesc: 'lorem ipsum lorem ipsum lorem ipsum',
+        tags: [ 'developer', 'web' ],
+        likesCount: 27,
+        likes: {
+          user: true,
+          skdjbfjsdbnfkjsdnfksdjn: true,
+          sakjdbsajkbdsakjbdjkasb: true
+        },
+        favsCount: 10,
+        favs: {
+          skdjbfjsdbnfkjsdnfksdjn: true,
+          sakjdbsajkbdsakjbdjkasb: true
+        },
+        media: {
+          mainImg: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg'
+        }
+      },
+      {
+        id: '3456789',
+        name: 'Recurso 2',
+        shortDesc: 'lorem2 ipsum lorem2 ipsum lorem2 ipsum',
+        tags: [ 'developer', 'web' ],
+        likesCount: 27,
+        likes: {
+          skdjbfjsdbnfkjsdnfksdjn: true,
+          sakjdbsajkbdsakjbdjkasb: true
+        },
+        favsCount: 10,
+        favs: {
+          user: true,
+          skdjbfjsdbnfkjsdnfksdjn: true,
+          sakjdbsajkbdsakjbdjkasb: true
+        },
+        media: {
+          mainImg: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg'
+        }
+      }
+    ]
+  })
+}
+</script>
