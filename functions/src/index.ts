f18d0ab3429d02ce3a78f3922831cfcdfc4f088a
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-02-27 23:40:44 +0100

    ./

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import { tmpdir } from 'os'
 import { join, dirname } from 'path'
 import * as sharp from 'sharp'
 import * as fs from 'fs-extra'
-import onSignUp from 'onSignUp'
+import onSignUp from './onSignUp'
 import * as algoliasearch from 'algoliasearch'
 const env = functions.config()
 const client = algoliasearch(env.algolia.appid, env.algolia.apikey)
