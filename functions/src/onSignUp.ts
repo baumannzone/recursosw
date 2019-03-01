e719255b637fb196b0d1cb914ab59274e8a565f2
Robin Giles Ribera <robinchogiles@gmail.com>
2019-03-01 23:14:31 +0100

    fix functions/src/onSignUp.ts

--- functions/src/onSignUp.ts
+++ functions/src/onSignUp.ts
@@ -4,11 +4,9 @@ function onSignUp (user, context) {
   admin.firestore().doc(`/users/${user.uid}`).set({
     id: user.uid,
     createdAt: new Date(),
-    email: user.email || user.email || '',
-    photo: user.photoURL || user.photoURL || '',
-    photoUrl: user.photoURL || user.photoURL || '',
+    email: user.email || '',
+    photoUrl: user.photoURL || '',
     displayName: user.displayName || user.email.split('@')[0] || 'Stranger',
-    name: user.displayName || user.email.split('@')[0] || 'Stranger',
     roles: {
       user: true
     },
