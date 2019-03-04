b95b4d9ee5100165e8efad160eef1bc3e3560b49
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-03-04 19:06:10 +0100

    WIP2

--- src/views/Admin/Users/UserCard.vue
+++ src/views/Admin/Users/UserCard.vue
@@ -12,70 +12,27 @@
             <div>{{ user.email }}</div>
           </div>
           <div class="bottom">
-            <div>{{ user.roles }}</div>
+            <user-roles :roles="user.roles" />
           </div>
         </div>
     </div>
-    <!-- <div class="resource pa-2 mb-2">
-      <div class="resource-img">
-        <img :src="data.media['128'] || data.media.mainImg" width="85px"/>
-      </div>
-      <div class="resource-content pl-3">
-        <div class="top">
-          <div class="title"><a class="resource-title" @click="go(data.id)">{{ data.name }}</a></div>
-          <div>{{ data.shortDesc }}</div>
-          <template v-if="$store.getters.isAuthenticated">
-            <button class="red--text" @click="deleteResource">Delete <small>[{{ data.id }}]</small></button>
-          </template>
-        </div>
-        <div class="bottom">
-          <div class="tags">
-            <v-chip
-              label
-              small
-              v-for="(tag, idx) in data.tags"
-              :key="idx"
-              text-color
-              @click="goTo(tag)"
-            >
-              {{ tag }}
-            </v-chip>
-          </div>
-          <div class="actions" v-if="isAuthenticated">
-            <v-chip outline small class="mr-1" @click="likeResource()">
-              <v-icon :color="likeColor[!!data.liked]">
-                thumb_up
-              </v-icon>
-              <span style="padding-left:5px">{{ data.likesCount }}</span>
-            </v-chip>
-            <v-chip outline small @click="favResource()">
-              <v-icon :color="favColor[!!data.favourited]">
-                {{ favIcon[data.favourited] }}
-              </v-icon>
-            </v-chip>
-          </div>
-          <div v-else>
-            <v-chip outline small disabled>
-              <v-icon>thumb_up</v-icon>
-              <span class="pl-1">{{ data.likesCount }}</span>
-            </v-chip>
-          </div>
-        </div>
-      </div>
-    </div> -->
   </v-card>
 </template>
 
 <script>
 import { db } from '@/config'
+import UserRoles from './UserCardRoles.vue'
 
 export default {
-  name: 'AdminUsers',
+  name: 'UserCard',
   props: {
     user: {
       type: Object,
       required: true
     }
+  },
+  components: {
+    UserRoles
   },
   data: () => {
     return {}
---
+++ src/views/Admin/Users/UserCardRoles.vue
@@ -0,0 +1,25 @@
+<template>
+  <span> R: {{ roles }} </span>
+</template>
+
+<script>
+
+export default {
+  name: 'UserCardRoles',
+  props: {
+    roles: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+
+</script>
+
+<style scoped lang="stylus">
+</style>
